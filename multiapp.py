516af6559883e0e5997203a32684dbe1e24f5feb
ngatttnl <62821812+ngatttnl@users.noreply.github.com>
2022-02-04 12:06:33 +0700

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -174,7 +174,6 @@ class MultiApp:
         """
         st.markdown(css_menu, unsafe_allow_html=True)
         ex_menu = """
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <div class="wrapMenuHeader">
                 <input type="checkbox" hidden id="collapseMenuMoile">
                 <div class="wrapLogoMenu" style="color: white;">
